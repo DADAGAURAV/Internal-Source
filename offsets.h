43cc0af2e6aa8487eaa5766209fd601011b81643
GaneshaVip <90498852+GaneshaVip@users.noreply.github.com>
2022-11-12 20:49:34 +0530

    Add files via upload

---
+++ offsets.h
@@ -0,0 +1,10 @@
+#pragma once
+namespace Offsets
+{
+	constexpr auto
+		Key = 0x9282978,
+		State = 0x9282940,
+		ProcessEvent = 0x3520430,
+		StaticFindObject = 0x35610e0,
+		TriggerVEH = 0x1D9DC50;
+}
