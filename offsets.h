726f24667da96911c88610c3c84465d8d5bf06d5
GaneshaVip <90498852+GaneshaVip@users.noreply.github.com>
2022-11-12 20:50:07 +0530

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -2,9 +2,10 @@
 namespace Offsets
 {
 	constexpr auto
-		Key = 0x9282978,
-		State = 0x9282940,
-		ProcessEvent = 0x3520430,
-		StaticFindObject = 0x35610e0,
-		TriggerVEH = 0x1D9DC50;
+		Key = 0x8FB80F8,
+		State = 0x8FB80C0,
+		ProcessEvent = 0x32F68B0,
+		StaticFindObject = 0x3337740,
+		TriggerVEH = 0x1D9DC50,
+		GetActorBounds = 0x4760F50;
 }
