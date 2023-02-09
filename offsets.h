5ec7f884f4a16e75c951f5471826ad3986ab609a
DADAGAURAV <90498852+DADAGAURAV@users.noreply.github.com>
2023-02-09 20:35:39 +0630

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -8,5 +8,6 @@ namespace Offsets
 		ProcessEvent = 0x33D1230,
 		StaticFindObject = 0x340F440,
 		TriggerVEH = 0x1BF5BC0,
+                ProjectToWorld = 0x4BA0050,
 		GetActorBounds = 0x4838720;  //void GetActorBounds(bool bOnlyCollidingComponents, struct FVector& Origin, struct FVector& BoxExtent, bool bIncludeFromChildActors); // Function Engine.Actor.GetActorBounds // (Final|Native|Public|HasOutParms|HasDefaults|BlueprintCallable|BlueprintPure|Const)
 }
